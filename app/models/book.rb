c6b9571611840ac564ed051c33e4f204fa5fc826
rachelbookhout <rbookhou@gmail.com>
2014-09-10 15:46:21 -0400

    Added validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+ validates_numericality_of :ranking, :less_than_or_equal_to=>100
  has_many :checkouts
  has_many :categorizations
  has_many :categories, through: :categorizations
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
+ validates :name, uniqueness:true
  has_many :categorizations
  has_many :books, through: :categorizations
 
