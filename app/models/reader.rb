1b0f10c2b5faf1700e1f2fdbd6f59c595f6172cf
rachelbookhout <rbookhou@gmail.com>
2014-09-10 14:31:52 -0400

    Split first and last names

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,4 +1,14 @@
 class Reader < ActiveRecord::Base
 has_many :books, through: :checkouts
+
+  def create_first_last_names
+    readers = Reader.all
+    readers.each do |reader|
+      reader_name = reader.name.split
+      reader.update(first_name: "#{reader_name[0]}")
+      reader.update(last_name: "#{reader_name[1]}")
+    end
+  end
+
 end
 
